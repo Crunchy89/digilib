bcfbddbccd912c96d09e7bd74818f54c5405e7aa
crunchy89 <rocker.hunt@gmail.com>
2023-02-28 01:04:36 +0800

    adding dashboard

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+    public function index()
+    {
+        $active = "dashboard";
+        $title = "dashboard";
+        return view("admin.dashboard", compact("active", "title"));
+    }
+}
